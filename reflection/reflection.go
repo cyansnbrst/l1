93b51265fea71a32516ae7d6c9110959c32e4103
Ekaterina <litra1122@gmail.com>
2024-12-03 16:07:37 +0300

    tasks 1-4

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,47 +1,17 @@
 package main
 
-import "fmt"
-
-func generator(nums ...int) <-chan int {
-	out := make(chan int)
-	go func() {
-		for _, n := range nums {
-			out <- n
-		}
-		close(out)
-	}()
-
-	return out
-}
-
-func multiplyByTwo(in <-chan int) <-chan int {
-	out := make(chan int)
-	go func() {
-		for n := range in {
-			out <- n * 2
-		}
-		close(out)
-	}()
-	return out
-}
-
-func square(in <-chan int) <-chan int {
-	out := make(chan int)
-	go func() {
-		for n := range in {
-			out <- n * n
-		}
-		close(out)
-	}()
-	return out
-}
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	numbers := generator(1, 2, 3, 4, 5)
-	multiplied := multiplyByTwo(numbers)
-	squared := square(multiplied)
-
-	for result := range squared {
-		fmt.Println(result)
+	for i := 0; i <= 20; i++ {
+		i := i
+		go func() {
+			fmt.Print(i, " ")
+		}()
 	}
+	time.Sleep(time.Second)
+	fmt.Println()
 }
