9664416fbf3ed6b817c2c3e7572eec45b621f07f
Ekaterina <litra1122@gmail.com>
2024-12-09 00:16:17 +0300

    task 16

---
+++ 16/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func quickSort(array []int) []int {
+	if len(array) <= 1 {
+		return array
+	}
+
+	pivotIdx := len(array) / 2
+	pivot := array[pivotIdx]
+
+	var left, middle, right []int
+
+	for _, num := range array {
+		switch {
+		case num < pivot:
+			left = append(left, num)
+		case num == pivot:
+			middle = append(middle, num)
+		case num > pivot:
+			right = append(right, num)
+		}
+	}
+
+	result := append(quickSort(left), middle...)
+	result = append(result, quickSort(right)...)
+
+	return result
+}
+
+func main() {
+	arr := []int{64, 34, 25, 12, 22, 11, 90, 34}
+
+	fmt.Println("sorted array:", quickSort(arr))
+}
