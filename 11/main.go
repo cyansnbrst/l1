7bd0e6a8a9e9180deb2ae72f2ff83af8349425d0
Ekaterina <litra1122@gmail.com>
2024-12-08 23:28:07 +0300

    task 11

--- 11/main.go
+++ 11/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	set1 := []int64{5, 3, -10, 8, 6, 8}
+	set1 := []int64{5, 3, -10, 8, 6}
 	set2 := []int64{8, 1, -10, 4, 2}
 
 	m := make(map[int64]int)
@@ -12,13 +12,13 @@ func main() {
 		m[elem]++
 	}
 
-	intersection := []int64{}
 	for _, elem := range set2 {
-		if count, exists := m[elem]; exists && count > 0 {
-			intersection = append(intersection, elem)
-			m[elem]--
-		}
+		m[elem]--
 	}
 
-	fmt.Println("Intersection:", intersection)
+	for key, value := range m {
+		if value == 0 {
+			fmt.Println(key)
+		}
+	}
 }
